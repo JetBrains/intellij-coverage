1241890b331c23e4880908d028ba4e2a66068279
Pavel Sher <Pavel.Sher@jetbrains.com>
2009-11-20 11:05:51 +0100

    use better hash algorithm

--- src/util/StringsPool.java
+++ src/util/StringsPool.java
@@ -1,17 +1,17 @@
 package com.intellij.rt.coverage.util;
 
-import gnu.trove.TIntObjectHashMap;
+import gnu.trove.TLongObjectHashMap;
 
 /**
  * @author Pavel.Sher
  */
 public class StringsPool {
-  private final static TIntObjectHashMap myReusableStrings = new TIntObjectHashMap(30000);
+  private final static TLongObjectHashMap myReusableStrings = new TLongObjectHashMap(30000);
 
   public static String getFromPool(String value) {
     if (value == null) return null;
 
-    final int hash = value.hashCode();
+    final long hash = StringHash.calc(value);
     String reused = (String) myReusableStrings.get(hash);
     if (reused != null) return reused;
     // new String() is required because value often is passed as substring which has a reference to original char array
