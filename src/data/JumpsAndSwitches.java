fd4988f4f4ab25a1951bcb83a3aa2e32663d6f6b
Pavel Sher <Pavel.Sher@jetbrains.com>
2009-12-28 18:13:29 +0100

    reduce memory usage even more: one reference instead of four

---
+++ src/data/JumpsAndSwitches.java
@@ -0,0 +1,127 @@
+package com.intellij.rt.coverage.data;
+
+import com.intellij.rt.coverage.util.CoverageIOUtil;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Pavel.Sher
+ */
+public class JumpsAndSwitches implements CoverageData {
+  private List myJumps;
+  private JumpData[] myJumpsArray;
+
+  private List mySwitches;
+  private SwitchData[] mySwitchesArray;
+
+  public List getJumps() {
+    return myJumps;
+  }
+
+  public List getSwitches() {
+    return mySwitches;
+  }
+
+  public JumpData addJump(final int jump) {
+    if (myJumps == null) myJumps = new ArrayList();
+    if (myJumps.size() <= jump) {
+      for (int i = myJumps.size(); i <= jump; i++){
+        myJumps.add(new JumpData());
+      }
+    }
+    return (JumpData)myJumps.get(jump);
+  }
+
+  public JumpData getJumpData(int jump) {
+    return myJumps == null && myJumpsArray == null ? null : myJumpsArray[jump];
+  }
+
+  public SwitchData addSwitch(final int switchNumber, final int[] keys) {
+    if (mySwitches == null) mySwitches = new ArrayList();
+    final SwitchData switchData = new SwitchData(keys);
+    if (mySwitches.size() <= switchNumber) {
+      for(int i = mySwitches.size(); i < switchNumber; i++) {
+        mySwitches.add(new SwitchData(new int[0]));
+      }
+      if (mySwitches.size() == switchNumber) {
+        mySwitches.add(switchData);
+      }
+    }
+    return (SwitchData)mySwitches.get(switchNumber);
+  }
+
+  public SwitchData getSwitchData(int switchNumber) {
+    return mySwitches == null && mySwitchesArray == null ? null : mySwitchesArray[switchNumber];
+  }
+
+  public void save(final DataOutputStream os) throws IOException {
+    CoverageIOUtil.writeINT(os, myJumpsArray != null ? myJumpsArray.length : 0);
+    if (myJumpsArray != null) {
+      for (int j = 0; j < myJumpsArray.length; j++) {
+        myJumpsArray[j].save(os);
+      }
+    }
+    CoverageIOUtil.writeINT(os, mySwitchesArray != null ? mySwitchesArray.length : 0);
+    if (mySwitchesArray != null) {
+      for (int s = 0; s < mySwitchesArray.length; s++) {
+        mySwitchesArray[s].save(os);
+      }
+    }
+  }
+
+  public void removeJump(final int jump) {
+    if (jump > 0 && jump <= myJumps.size()) {
+      myJumps.remove(jump - 1);
+    }
+  }
+
+  public void fillArrays() {
+    if (myJumps != null) {
+      myJumpsArray = new JumpData[myJumps.size()];
+      for (int i = 0; i < myJumps.size(); i++) {
+        myJumpsArray[i] = (JumpData)myJumps.get(i);
+      }
+      myJumps = null;
+    }
+    if (mySwitches != null) {
+      mySwitchesArray = new SwitchData[mySwitches.size()];
+      for (int i = 0; i < mySwitches.size(); i++) {
+        mySwitchesArray[i] = (SwitchData)mySwitches.get(i);
+      }
+      mySwitches = null;
+    }
+  }
+
+  public void merge(final CoverageData data) {
+    JumpsAndSwitches jumpsData = (JumpsAndSwitches)data;
+    if (jumpsData.myJumps != null) {
+      if (myJumps == null) {
+        myJumps = jumpsData.myJumps;
+      }
+      else if (jumpsData.myJumps != null) {
+        for (int i = Math.min(myJumps.size(), jumpsData.myJumps.size()) - 1; i >= 0; i--) {
+          ((JumpData)myJumps.get(i)).merge((JumpData) jumpsData.myJumps.get(i));
+        }
+        for (int i = Math.min(myJumps.size(), jumpsData.myJumps.size()); i < jumpsData.myJumps.size(); i++) {
+          myJumps.add(jumpsData.myJumps.get(i));
+        }
+      }
+    }
+    if (jumpsData.mySwitches != null) {
+      if (mySwitches == null) {
+        mySwitches = jumpsData.mySwitches;
+      }
+      else if (jumpsData.mySwitches != null) {
+        for (int i = Math.min(mySwitches.size(), jumpsData.mySwitches.size()) - 1; i >= 0; i--) {
+          ((SwitchData)mySwitches.get(i)).merge((SwitchData) jumpsData.mySwitches.get(i));
+        }
+        for (int i = Math.min(mySwitches.size(), jumpsData.mySwitches.size()); i < jumpsData.mySwitches.size(); i++) {
+          mySwitches.add(jumpsData.mySwitches.get(i));
+        }
+      }
+    }
+  }
+}
--- src/data/LineData.java
+++ src/data/LineData.java
@@ -4,7 +4,6 @@ import com.intellij.rt.coverage.util.CoverageIOUtil;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
@@ -14,15 +13,11 @@ public class LineData implements CoverageData {
 
   private int myHits = 0;
 
-  private List myJumps;
-  private JumpData[] myJumpsArray;
-
-  private List mySwitches;
-  private SwitchData[] mySwitchesArray;
-
   private byte myStatus = -1;
   private String myUniqueTestName = null;
   private boolean myMayBeUnique = true;
+
+  private JumpsAndSwitches myJumpsAndSwitches;
 
   public LineData(final int line, final String desc) {
     myLineNumber = line;
@@ -38,14 +33,23 @@ public class LineData implements CoverageData {
     return myHits;
   }
 
+  JumpsAndSwitches getJumpsAndSwitches() {
+    if (myJumpsAndSwitches == null) {
+      myJumpsAndSwitches = new JumpsAndSwitches();
+    }
+    return myJumpsAndSwitches;
+  }
+
   public int getStatus() {
     if (myStatus != -1) return myStatus;
     if (myHits == 0) {
       myStatus = LineCoverage.NONE;
       return myStatus;
     }
-    if (myJumps != null) {
-      for (Iterator it = myJumps.iterator(); it.hasNext();) {
+
+    List jumps = getJumpsAndSwitches().getJumps();
+    if (jumps != null) {
+      for (Iterator it = jumps.iterator(); it.hasNext();) {
         final JumpData jumpData = (JumpData)it.next();
         if ((jumpData.getFalseHits() > 0 ? 1 : 0) + (jumpData.getTrueHits() > 0 ? 1 : 0) < 2){
           myStatus = LineCoverage.PARTIAL;
@@ -53,8 +57,10 @@ public class LineData implements CoverageData {
         }
       }
     }
-    if (mySwitches != null) {
-      for (Iterator it = mySwitches.iterator(); it.hasNext();) {
+
+    List switches = getJumpsAndSwitches().getSwitches();
+    if (switches != null) {
+      for (Iterator it = switches.iterator(); it.hasNext();) {
         final SwitchData switchData = (SwitchData)it.next();
         if (switchData.getDefaultHits() == 0){
           myStatus = LineCoverage.PARTIAL;
@@ -78,65 +84,23 @@ public class LineData implements CoverageData {
     CoverageIOUtil.writeUTF(os, myUniqueTestName != null ? myUniqueTestName : "");
     CoverageIOUtil.writeINT(os, myHits);
     if (myHits > 0) {
-      CoverageIOUtil.writeINT(os, myJumpsArray != null ? myJumpsArray.length : 0);
-      if (myJumpsArray != null) {
-        for (int j = 0; j < myJumpsArray.length; j++) {
-          myJumpsArray[j].save(os);
-        }
-      }
-      CoverageIOUtil.writeINT(os, mySwitchesArray != null ? mySwitchesArray.length : 0);
-      if (mySwitchesArray != null) {
-        for (int s = 0; s < mySwitchesArray.length; s++) {
-          mySwitchesArray[s].save(os);
-        }
-      }
+      getJumpsAndSwitches().save(os);
     }
   }
 
   public void merge(final CoverageData data) {
     LineData lineData = (LineData)data;
     myHits += lineData.myHits;
-    if (lineData.myJumps != null) {
-      if (myJumps == null) {
-        myJumps = lineData.myJumps;
-      }
-      else if (lineData.myJumps != null) {
-        for (int i = Math.min(myJumps.size(), lineData.myJumps.size()) - 1; i >= 0; i--) {
-          ((JumpData)myJumps.get(i)).merge((JumpData)lineData.myJumps.get(i));
-        }
-        for (int i = Math.min(myJumps.size(), lineData.myJumps.size()); i < lineData.myJumps.size(); i++) {
-          myJumps.add(lineData.myJumps.get(i));
-        }
-      }
-    }
-    if (lineData.mySwitches != null) {
-      if (mySwitches == null) {
-        mySwitches = lineData.mySwitches;
-      }
-      else if (lineData.mySwitches != null) {
-        for (int i = Math.min(mySwitches.size(), lineData.mySwitches.size()) - 1; i >= 0; i--) {
-          ((SwitchData)mySwitches.get(i)).merge((SwitchData)lineData.mySwitches.get(i));
-        }
-        for (int i = Math.min(mySwitches.size(), lineData.mySwitches.size()); i < lineData.mySwitches.size(); i++) {
-          mySwitches.add(lineData.mySwitches.get(i));
-        }
-      }
-    }
+    getJumpsAndSwitches().merge(lineData.getJumpsAndSwitches());
     if (lineData.myMethodSignature != null) myMethodSignature = lineData.myMethodSignature;
   }
 
   public JumpData addJump(final int jump) {
-    if (myJumps == null) myJumps = new ArrayList();
-    if (myJumps.size() <= jump) {
-      for (int i = myJumps.size(); i <= jump; i++){
-        myJumps.add(new JumpData());
-      }
-    }
-    return (JumpData)myJumps.get(jump);
+    return getJumpsAndSwitches().addJump(jump);
   }
 
   public JumpData getJumpData(int jump) {
-    return myJumps == null && myJumpsArray == null ? null : myJumpsArray[jump];
+    return getJumpsAndSwitches().getJumpData(jump);
   }
 
   public void touchBrunch(final int jump, final boolean hit) {
@@ -152,17 +116,11 @@ public class LineData implements CoverageData {
   }
 
   public SwitchData addSwitch(final int switchNumber, final int[] keys) {
-    if (mySwitches == null) mySwitches = new ArrayList();
-    final SwitchData switchData = new SwitchData(keys);
-    if (mySwitches.size() <= switchNumber) {
-      for(int i = mySwitches.size(); i < switchNumber; i++) {
-        mySwitches.add(new SwitchData(new int[0]));
-      }
-      if (mySwitches.size() == switchNumber) {
-        mySwitches.add(switchData);
-      }
-    }
-    return (SwitchData)mySwitches.get(switchNumber);
+    return getJumpsAndSwitches().addSwitch(switchNumber, keys);
+  }
+
+  public SwitchData getSwitchData(int switchNumber) {
+    return getJumpsAndSwitches().getSwitchData(switchNumber);
   }
 
   public SwitchData addSwitch(final int switchNumber, final int min, final int max) {
@@ -178,10 +136,6 @@ public class LineData implements CoverageData {
     if (switchData != null) {
       switchData.touch(key);
     }
-  }
-
-  public SwitchData getSwitchData(int switchNumber) {
-    return mySwitches == null && mySwitchesArray == null ? null : mySwitchesArray[switchNumber];
   }
 
   public int getLineNumber() {
@@ -213,11 +167,11 @@ public class LineData implements CoverageData {
   }
 
   public List getJumps() {
-    return myJumps;
+    return getJumpsAndSwitches().getJumps();
   }
 
   public List getSwitches() {
-    return mySwitches;
+    return getJumpsAndSwitches().getSwitches();
   }
 
   public void setHits(final int hits) {
@@ -240,25 +194,10 @@ public class LineData implements CoverageData {
   }
 
   public void removeJump(final int jump) {
-    if (jump > 0 && jump <= myJumps.size()) {
-      myJumps.remove(jump - 1);
-    }
+    getJumpsAndSwitches().removeJump(jump);
   }
 
   public void fillArrays() {
-    if (myJumps != null) {
-      myJumpsArray = new JumpData[myJumps.size()];
-      for (int i = 0; i < myJumps.size(); i++) {
-        myJumpsArray[i] = (JumpData)myJumps.get(i);
-      }
-      myJumps = null;
-    }
-    if (mySwitches != null) {
-      mySwitchesArray = new SwitchData[mySwitches.size()];
-      for (int i = 0; i < mySwitches.size(); i++) {
-        mySwitchesArray[i] = (SwitchData)mySwitches.get(i);
-      }
-      mySwitches = null;
-    }
+    getJumpsAndSwitches().fillArrays();
   }
 }
