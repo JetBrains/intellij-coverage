d9c75ae06a8da94fad85de765c8d27112037e451
Pavel Sher <Pavel.Sher@jetbrains.com>
2010-03-02 09:42:19 +0100

    attempt to fix URLs

--- src/main/CoveragePremain.java
+++ src/main/CoveragePremain.java
@@ -7,6 +7,7 @@ package com.intellij.rt.coverage.main;
 import java.io.File;
 import java.lang.instrument.Instrumentation;
 import java.lang.reflect.Method;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.Arrays;
@@ -15,15 +16,19 @@ public class CoveragePremain {
   public static void premain(String argsString, Instrumentation instrumentation) throws Exception {
     final File lib = new File(getArchivePath()).getParentFile();
     final URL[] urls = new URL[5];
-    urls[0] = new File(lib, "instrumenter.jar").toURI().toURL();
-    urls[1] = new File(lib, "asm-commons.jar").toURI().toURL();
-    urls[2] = new File(lib, "asm-tree-3.0.jar").toURI().toURL();
-    urls[3] = new File(lib, "asm.jar").toURI().toURL();
-    urls[4] = new File(lib, "trove4j.jar").toURI().toURL();
+    urls[0] = fileToURL(new File(lib, "instrumenter.jar"));
+    urls[1] = fileToURL(new File(lib, "asm-commons.jar"));
+    urls[2] = fileToURL(new File(lib, "asm-tree-3.0.jar"));
+    urls[3] = fileToURL(new File(lib, "asm.jar"));
+    urls[4] = fileToURL(new File(lib, "trove4j.jar"));
     System.out.println("Classloader URLs: " + Arrays.asList(urls));
     final Class instrumentator = Class.forName("com.intellij.rt.coverage.instrumentation.Instrumentator", true, new URLClassLoader(urls));
     final Method premainMethod = instrumentator.getDeclaredMethod("premain", new Class[]{String.class, Instrumentation.class});
     premainMethod.invoke(null, new Object[] {argsString, instrumentation});
+  }
+
+  private static URL fileToURL(final File file) throws MalformedURLException {
+    return file.getAbsoluteFile().toURI().toURL();
   }
 
   private static String getArchivePath() {
