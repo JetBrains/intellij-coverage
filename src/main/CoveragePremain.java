5ee0df02992cde14d6005c8ef5d68108c6949dc5
Anna Kozlova <Anna.Kozlova@jetbrains.com>
2010-03-02 12:25:43 +0100

    cleanup

--- src/main/CoveragePremain.java
+++ src/main/CoveragePremain.java
@@ -12,7 +12,6 @@ import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
-import java.util.Arrays;
 
 public class CoveragePremain {
 
@@ -24,7 +23,6 @@ public class CoveragePremain {
     urls[2] = fileToURL(new File(lib, "asm-tree-3.0.jar"));
     urls[3] = fileToURL(new File(lib, "asm.jar"));
     urls[4] = fileToURL(new File(lib, "trove4j.jar"));
-    System.out.println("Classloader URLs: " + Arrays.asList(urls));
     final Class instrumentator = Class.forName("com.intellij.rt.coverage.instrumentation.Instrumentator", true, new URLClassLoader(urls));
     final Method premainMethod = instrumentator.getDeclaredMethod("premain", new Class[]{String.class, Instrumentation.class});
     premainMethod.invoke(null, new Object[] {argsString, instrumentation});
