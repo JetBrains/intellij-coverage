0683b88e4ef52ae25fe02e8d74b817a4e1dfcd1e
zuevmaxim <max-zu99@mail.ru>
2020-10-06 15:13:18 +0300

    Add first Kotlin coverage tests (#7)
    * Add first Kotlin tests
    
    * Update junit version
    
    * Use temporary file for tests
    
    * Update assertEquals method
    
    * Avoid using enum
    
    * Use runCoverage method
    
    * Remove assertEqualsClassLines extension
    
    Co-authored-by: Maksim Zuev <maksim.zuev@jetbrains.com>

---
+++ test-kotlin/src/com/intellij/rt/coverage/kotlin/KotlinCoverageStatusTest.kt
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2000-2020 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.intellij.rt.coverage.kotlin
+
+
+import com.intellij.rt.coverage.assertEqualsClassLines
+import com.intellij.rt.coverage.data.LineCoverage.FULL
+import com.intellij.rt.coverage.runWithCoverage
+import org.junit.After
+import org.junit.Before
+import org.junit.Ignore
+import org.junit.Test
+import java.io.File
+
+
+class KotlinCoverageStatusTest {
+    private lateinit var myDataFile: File
+
+    @Before
+    fun setUp() {
+        myDataFile = createTempFile("test")
+    }
+
+    @After
+    fun tearDown() {
+        myDataFile.delete()
+    }
+
+    @Test
+    @Ignore("Not implemented")
+    fun testDefaultArgs() = testClassCoverage("defaultArgs", mapOf(
+            20 to FULL,
+            24 to FULL,
+            25 to FULL
+    ))
+
+
+    @Test
+    @Ignore("Not implemented")
+    fun testInline() = testClassCoverage("inline", mapOf(
+            20 to FULL,
+            21 to FULL,
+            25 to FULL,
+            26 to FULL
+    ))
+
+    @Test
+    @Ignore("Not implemented")
+    fun testInlineInline() = testClassCoverage("inlineInline", mapOf(
+            20 to FULL,
+            21 to FULL,
+            22 to FULL,
+            26 to FULL,
+            27 to FULL,
+            31 to FULL,
+            32 to FULL
+    ))
+
+    @Test
+    @Ignore("Not implemented")
+    fun testReturn() = testClassCoverage("returnTest", mapOf(
+            20 to FULL
+    ))
+
+    private fun testClassCoverage(testName: String, expected: Map<Int, Byte>, sampling: Boolean = true) {
+        val project = runWithCoverage(myDataFile, testName, sampling)
+        assertEqualsClassLines(project, "kotlinTestData.$testName.TestKt", expected)
+    }
+}
---
+++ test-kotlin/src/com/intellij/rt/coverage/runner.kt
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2000-2020 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.intellij.rt.coverage
+
+import com.intellij.rt.coverage.data.ClassData
+import com.intellij.rt.coverage.data.LineData
+import com.intellij.rt.coverage.data.ProjectData
+import org.junit.Assert.assertEquals
+import java.io.File
+
+
+fun runWithCoverage(coverageDataFile: File, testName: String, sampling: Boolean): ProjectData {
+    val classPath = System.getProperty("java.class.path")
+    return CoverageStatusTest.runCoverage(classPath, coverageDataFile, ".*", "kotlinTestData.$testName.Test", sampling)
+}
+
+internal fun assertEqualsClassLines(project: ProjectData, className: String, expectedLines: Map<Int, Byte>) {
+    val classData = project.getClassData(className)!!
+    val lines = classData.getLinesData().associateBy({ it.lineNumber }, { it.status.toByte() })
+    assertEquals(statusToString(expectedLines), statusToString(lines))
+}
+
+private fun statusToString(lines: Map<Int, Byte>) = lines.mapValues {
+    when (it.value.toInt()) {
+        0 -> "NONE"
+        1 -> "PARTIAL"
+        else -> "FULL"
+    }
+}
+
+private fun ClassData.getLinesData() = lines.filterIsInstance(LineData::class.java).sortedBy { it.lineNumber }
