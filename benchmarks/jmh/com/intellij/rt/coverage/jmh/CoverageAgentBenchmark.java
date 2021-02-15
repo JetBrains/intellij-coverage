7ea93e0b4d4714585a564e1f94cb69d87d6aa3ea
Maksim Zuev <maksim.zuev@jetbrains.com>
2021-02-15 16:45:15 +0300

    Revert "Add apache tests"
    This reverts commit 52f1290d

--- benchmarks/jmh/com/intellij/rt/coverage/jmh/CoverageAgentBenchmark.java
+++ benchmarks/jmh/com/intellij/rt/coverage/jmh/CoverageAgentBenchmark.java
@@ -33,15 +33,4 @@ public class CoverageAgentBenchmark {
       System.setOut(original);
     }
   }
-
-  @Benchmark
-  public void apacheCollectionsTestCoverage() throws Exception {
-    PrintStream original = System.out;
-    try {
-      System.setOut(new PrintStream(new NullOutputStream()));
-      ApacheCollectionsTests.runTests();
-    } finally {
-      System.setOut(original);
-    }
-  }
 }
