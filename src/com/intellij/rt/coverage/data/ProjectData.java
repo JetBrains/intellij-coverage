f7a2bd572ee1448fa9f43a968390069885ccd413
Maksim Zuev <maksim.zuev@jetbrains.com>
2021-04-01 11:45:31 +0300

    Add size() method

--- src/com/intellij/rt/coverage/data/ProjectData.java
+++ src/com/intellij/rt/coverage/data/ProjectData.java
@@ -84,6 +84,10 @@ public class ProjectData implements CoverageData, Serializable {
     return mySampling;
   }
 
+  public int getClassesNumber() {
+    return myClasses.size();
+  }
+
   public static ProjectData createProjectData(final File dataFile,
                                               final ProjectData initialData,
                                               boolean traceLines,
@@ -396,6 +400,10 @@ public class ProjectData implements CoverageData, Serializable {
     private static final int DEFAULT_CAPACITY = 1000;
     private final IdentityClassData[] myIdentityArray = new IdentityClassData[POOL_SIZE];
     private final Map<String, ClassData> myClasses = createClassesMap();
+
+    public int size() {
+      return myClasses.size();
+    }
 
     public ClassData get(String name) {
       int idx = name.hashCode() & MASK;
