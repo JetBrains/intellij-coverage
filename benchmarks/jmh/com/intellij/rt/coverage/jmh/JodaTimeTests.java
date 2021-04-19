f2df97fb74d207d71bf66af1fa46fa8707576cab
Maksim Zuev <maksim.zuev@jetbrains.com>
2021-04-19 20:25:23 +0300

    Add coverage benchmarks (#38)
    * Fix running benchmarks
    
    * Move benchmarks into a separate module
    
    * Add apache tests
    
    * New instrumentation benchmarks

--- benchmarks/jmh/com/intellij/rt/coverage/jmh/JodaTimeTests.java
+++ benchmarks/jmh/com/intellij/rt/coverage/jmh/JodaTimeTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2018 JetBrains s.r.o.
+ * Copyright 2000-2020 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.intellij.rt.coverage.testDiscovery.jmh;
+package com.intellij.rt.coverage.jmh;
 
 import junit.framework.TestResult;
 import junit.textui.TestRunner;
@@ -24,22 +24,30 @@ import java.util.Locale;
 import java.util.TimeZone;
 
 public class JodaTimeTests {
-  public static void testDiscovery() throws Exception {
+  private static void runTests(TestRunner runner) throws Exception {
     // setup a time zone other than one tester is in
     TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 
     // setup a locale other than one the tester is in
     Locale.setDefault(new Locale("th", "TH"));
 
-    new TestRunner() {
+    runner.start(new String[]{
+        TestAllPackages.class.getName()
+    });
+  }
+
+  public static void testDiscovery() throws Exception {
+    runTests(new TestRunner() {
       @Override
       protected TestResult createTestResult() {
         TestResult result = super.createTestResult();
         result.addListener(new TestDiscoveryListener());
         return result;
       }
-    }.start(new String[]{
-        TestAllPackages.class.getName()
     });
+  }
+
+  public static void testCoverage() throws Exception {
+    runTests(new TestRunner());
   }
 }
