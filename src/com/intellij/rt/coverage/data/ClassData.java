fa5b7b5a7fb46d7881df19c86016a90e5a60643b
Maksim Zuev <maksim.zuev@jetbrains.com>
2021-05-26 19:27:21 +0300

    Clean up

--- src/com/intellij/rt/coverage/data/ClassData.java
+++ src/com/intellij/rt/coverage/data/ClassData.java
@@ -118,7 +118,6 @@ public class ClassData implements CoverageData {
     }
   }
 
-
   public void touchLine(int line) {
     myLineMask[line]++;
   }
--- src/com/intellij/rt/coverage/data/ProjectData.java
+++ src/com/intellij/rt/coverage/data/ProjectData.java
@@ -399,7 +399,6 @@ public class ProjectData implements CoverageData, Serializable {
       }
     }
   }
-
   // ----------------------------------------------------------------------------------------------- //
 
   private static class MethodCaller {
