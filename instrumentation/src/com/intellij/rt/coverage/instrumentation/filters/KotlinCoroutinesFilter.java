0f6da07631a2f129bc252742d6fd4cbc95703f32
Maksim Zuev <maksim.zuev@jetbrains.com>
2021-11-09 15:34:16 +0300

    Clean up KotlinCoroutinesFilter

--- instrumentation/src/com/intellij/rt/coverage/instrumentation/filters/KotlinCoroutinesFilter.java
+++ instrumentation/src/com/intellij/rt/coverage/instrumentation/filters/KotlinCoroutinesFilter.java
@@ -34,7 +34,6 @@ public abstract class KotlinCoroutinesFilter extends MethodVisitor {
   private int myCoroutinesSuspendedIndex = -1;
   private int myLine = -1;
   private boolean myHadLineDataBefore = false;
-  private boolean myHasExecutableCode = false;
   private final Instrumenter myContext;
 
   public KotlinCoroutinesFilter(MethodVisitor methodVisitor, Instrumenter instrumenter) {
@@ -66,13 +65,11 @@ public abstract class KotlinCoroutinesFilter extends MethodVisitor {
     myHadLineDataBefore = myContext.getLineData(line) != null;
     super.visitLineNumber(line, start);
     myLine = line;
-    myHasExecutableCode = false;
   }
 
   @Override
   public void visitMethodInsn(int opcode, String owner, String name, String descriptor, boolean isInterface) {
     super.visitMethodInsn(opcode, owner, name, descriptor, isInterface);
-    myHasExecutableCode = true;
 
     boolean getCoroutinesSuspendedVisited = opcode == Opcodes.INVOKESTATIC
         && owner.equals("kotlin/coroutines/intrinsics/IntrinsicsKt")
@@ -93,7 +90,6 @@ public abstract class KotlinCoroutinesFilter extends MethodVisitor {
   @Override
   public void visitVarInsn(int opcode, int var) {
     super.visitVarInsn(opcode, var);
-    myHasExecutableCode = true;
     if (!myStoreCoroutinesSuspendedVisited && myGetCoroutinesSuspendedVisited && opcode == Opcodes.ASTORE) {
       myStoreCoroutinesSuspendedVisited = true;
       myCoroutinesSuspendedIndex = var;
@@ -110,7 +106,6 @@ public abstract class KotlinCoroutinesFilter extends MethodVisitor {
   @Override
   public void visitJumpInsn(int opcode, Label label) {
     super.visitJumpInsn(opcode, label);
-    myHasExecutableCode = true;
     boolean compareWithCoroutinesSuspend = myLoadCoroutinesSuspendedVisited || myGetCoroutinesSuspendedVisited;
     if (compareWithCoroutinesSuspend && mySuspendCallVisited && opcode == Opcodes.IF_ACMPNE) {
       onIgnoredJump();
@@ -122,7 +117,6 @@ public abstract class KotlinCoroutinesFilter extends MethodVisitor {
   @Override
   public void visitFieldInsn(int opcode, String owner, String name, String descriptor) {
     super.visitFieldInsn(opcode, owner, name, descriptor);
-    myHasExecutableCode = true;
     final boolean labelVisited = name.equals("label")
         && descriptor.equals("I")
         && Type.getObjectType(owner).getClassName().startsWith(myContext.getClassName());
@@ -136,7 +130,6 @@ public abstract class KotlinCoroutinesFilter extends MethodVisitor {
   @Override
   public void visitTableSwitchInsn(int min, int max, Label dflt, Label... labels) {
     super.visitTableSwitchInsn(min, max, dflt, labels);
-    myHasExecutableCode = true;
     if (myLoadStateLabelVisited) {
       onIgnoredSwitch(dflt, labels);
       if (!myHadLineDataBefore) {
@@ -149,52 +142,9 @@ public abstract class KotlinCoroutinesFilter extends MethodVisitor {
   @Override
   public void visitInsn(int opcode) {
     super.visitInsn(opcode);
-    myHasExecutableCode = true;
     // ignore generated return on the first line
     if (opcode == Opcodes.ARETURN && myLoadCoroutinesSuspendedVisited && !myHadLineDataBefore) {
       onIgnoredLine(myLine);
     }
-  }
-
-  @Override
-  public void visitTypeInsn(int opcode, String type) {
-    super.visitTypeInsn(opcode, type);
-    myHasExecutableCode = true;
-  }
-
-  @Override
-  public void visitMultiANewArrayInsn(String descriptor, int numDimensions) {
-    super.visitMultiANewArrayInsn(descriptor, numDimensions);
-    myHasExecutableCode = true;
-  }
-
-  @Override
-  public void visitInvokeDynamicInsn(String name, String descriptor, Handle bootstrapMethodHandle, Object... bootstrapMethodArguments) {
-    super.visitInvokeDynamicInsn(name, descriptor, bootstrapMethodHandle, bootstrapMethodArguments);
-    myHasExecutableCode = true;
-  }
-
-  @Override
-  public void visitIntInsn(int opcode, int operand) {
-    super.visitIntInsn(opcode, operand);
-    myHasExecutableCode = true;
-  }
-
-  @Override
-  public void visitIincInsn(int var, int increment) {
-    super.visitIincInsn(var, increment);
-    myHasExecutableCode = true;
-  }
-
-  @Override
-  public void visitLdcInsn(Object value) {
-    super.visitLdcInsn(value);
-    myHasExecutableCode = true;
-  }
-
-  @Override
-  public void visitLookupSwitchInsn(Label dflt, int[] keys, Label[] labels) {
-    super.visitLookupSwitchInsn(dflt, keys, labels);
-    myHasExecutableCode = true;
   }
 }
